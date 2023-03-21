c156faef41d43c6c9f3b216693a6ec61f5bc250a
Jp <jpgit300@gmail.com>
2023-03-21 19:35:26 +0000

    fixed

--- Api/dndvtt.api/Program.cs
+++ Api/dndvtt.api/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddScoped<IBoardFacade, BoardFacade>();
 builder.Services.AddScoped<IChatFacade, ChatFacade>();
 builder.Services.AddScoped<IPanelFacade, PanelFacade>();
 builder.Services.AddScoped<IToolsFacade, ToolsFacade>();
-builder.Services.AddSingleton<ITtmFacade, TtmFacade>();
+builder.Services.AddScoped<ITtmFacade, TtmFacade>();
 // SWAGGER
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
