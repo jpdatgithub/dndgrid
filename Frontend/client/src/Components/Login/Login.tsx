9452d4fd9918ccb11e1e2df06e5cc43dbb998b0b
Jp <jpgit300@gmail.com>
2023-03-24 16:01:33 +0000

    chá de minhapik

--- Frontend/client/src/Components/Login/Login.tsx
+++ Frontend/client/src/Components/Login/Login.tsx
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.scss';
+
+export interface ICredentials {
+  username: string,
+  password: string
+}
+
+async function loginUser(credentials: ICredentials) {
+  return fetch('http://localhost:8080/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
+ }
 
 export interface ILoginProps {
     setToken: Function
 }
 
 export default function Login(props: ILoginProps) {
+  const [username, setUserName] = useState(String);
+  const [password, setPassword] = useState(String);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const token = await loginUser({
+      username,
+      password
+    });
+    props.setToken(token);
+  }
+
   return(
     <div className="login-wrapper">
       <h1>Please Log In</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <p>Username</p>
-          <input type="text" />
+          <input type="text" onChange={e => setUserName(e.target.value)}/>
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input type="password" onChange={e => setPassword(e.target.value)}/>
         </label>
         <div>
           <button type="submit">Submit</button>
